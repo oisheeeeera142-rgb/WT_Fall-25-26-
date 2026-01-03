65db8448a9f62d88752a88b0c238123916160814
oisheeeeera142-rgb <oisheeeeera142@gmail.com>
2026-01-04 00:16:33 +0600

    Update all changes

--- Management/View/admindashboard.js
+++ Management/View/admindashboard.js
@@ -1,30 +1,22 @@
 function goPage(page) {
-    switch (page) {
-        case 'rooms':
-            window.location.href = 'rooms.php';
-            break;
-        case 'booking':
-            window.location.href = 'booking.php';
-            break;
-        case 'housekeeping':
-            window.location.href = 'housekeeping.php';
-            break;
-        case 'payment':
-            window.location.href = 'payment.php';
-            break;
-        case 'guestpayment':
-            window.location.href = 'guestpayment.php';
-            break;
-        case 'reviews':
-            window.location.href = 'reviews.php';
-            break;
-        case 'reports':
-            window.location.href = 'reports.php';
-            break;
-        case 'guest':
-            window.location.href = 'guest.php';
-            break;
-        default:
-            alert("Page not found!");
+    const pages = {
+        rooms: "rooms.php",
+        booking: "booking.php",
+        housekeeping: "housekeeping.php",
+        payment: "payment.php",
+        guestpayment: "guestpayment.php",
+        reviews: "reviews.php",
+        reports: "reports.php",
+        guest: "guest.php"
+    };
+
+    if (pages[page]) {
+        window.location.href = pages[page];
+    } else {
+        alert("Page not found!");
     }
+}
+
+function logout() {
+    window.location.href = "landingh.php";
 }
